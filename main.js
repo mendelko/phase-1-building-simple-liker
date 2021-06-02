21e9eca56c3fa30a1efef8bdbe379ebf9e4b26bc
Mendel <mkonikov123@gmail.com>
2021-06-02 19:54:30 -0400

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,48 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const glyph = document.querySelectorAll(".like-glyph");
 
+
+
+function likedPost(e){
+  if (e.target.innerHTML === EMPTY_HEART){
+    mimicServerCall()
+    .then(function(object){
+      e.target.innerHTML = FULL_HEART
+       e.target.classList.add("activated-heart");
+
+
+      })
+      .catch(function(error){
+        document.getElementById("modal").classList.remove("hidden")
+        document.getElementById("modal-message").innerHTML = error
+        setTimeout(function(){
+          document.getElementById("modal").classList.add("hidden")
+
+        }, 3000)
+
+      })
+    }else {
+      e.target.innerHTML = EMPTY_HEART
+      e.target.classList.remove("activated-heart")
+      
+
+    }
+}
+
+for (const like of glyph) {
+  like.addEventListener("click", likedPost);
+}
+
+
+
+
+
+
+  
+  
+  
 
 
 
